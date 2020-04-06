b4ca9abc6d8880266ab7e2663ca9403570b0a9db
thaomit18011998 <son73462385@gmail.com>
2020-04-06 22:29:00 +0700

    first commit

---
+++ Assets/Scripts/MapCreator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour
+{
+    public int rowCount = 10;
+    public int columCount = 10;
+    public int scale = 1;
+    public GameObject gridPrefab;
+    public Vector3 leftBottomLocation = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (gridPrefab)
+        {
+            Geneategrid();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Geneategrid()
+    {
+        for (int i = 0; i < columCount; i++)
+        {
+            for (int j = 0; j < rowCount; j++)
+            {
+                GameObject obj = Instantiate(gridPrefab, new Vector3(leftBottomLocation.x+scale*i, leftBottomLocation.y, leftBottomLocation.z+scale *j), Quaternion.identity);
+                obj.transform.SetParent(gameObject.transform);
+                obj.GetComponent<GridStats>().x = i;
+                obj.GetComponent<GridStats>().y = j;
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Transform particleEffect;
+    public Transform particleEffect2;
+    private Rigidbody rd;
+    private Vector3 rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        particleEffect2.transform.position = transform.position;
+        // Tạo di chuyển cho player
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rd.AddForce(movement * speed);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        // Khi player va chạm wall tao hieu ung
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Vector3 particlePos = new Vector3(transform.position.x, transform.position.y + 0.3f,transform.position.z);
+            Instantiate(particleEffect, particlePos, Quaternion.identity);
+         
+        }
+    }
+}
